ce2dcbb7a26fdb9891bf7143df0ca75edd3fe2dc
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-17 21:33:43 -0300

    Menu responsivo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+
 import miniLogo from '../../img/miniLogo_c.svg';
+import { CgMenuRightAlt, CgCloseR } from "react-icons/cg";
+import * as C from './styles'
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
     return(
-        <nav>
-            <img src={miniLogo} alt='Logo Checkspeech' />
-            <ul>
-                <li>
-                Soluções
-                </li>
-                <li>
-                Clientes
-                </li>
-                <li>
-                Preços
-                </li>
-                <li>
-                Contatos
-                </li>
-            </ul>
-        </nav>
+        <C.Nav>
+            <C.Logo src={miniLogo} alt='Logo Checkspeech' />
+            <C.Menu
+            showMenu={showMenu}
+            >
+                <C.ItemMenu
+                onClick={() => setShowMenu(!showMenu)}
+                >
+                    Início
+                </C.ItemMenu>
+                <C.ItemMenu
+                onClick={() => setShowMenu(!showMenu)}
+                >
+                    Soluções
+                </C.ItemMenu>
+                <C.ItemMenu
+                onClick={() => setShowMenu(!showMenu)}
+                >
+                    Preços
+                </C.ItemMenu>
+                <C.ItemMenu
+                onClick={() => setShowMenu(!showMenu)}
+                >
+                    Contatos
+                </C.ItemMenu>
+            </C.Menu>
+
+            <C.ButtonMenu onClick={() => setShowMenu(!showMenu)}>
+                <C.IconMenu>
+                    {
+                    showMenu?
+                        <CgCloseR/>:
+                        <CgMenuRightAlt>
+                            <C.Menu
+                            showMenu={showMenu}
+                            >
+                                <C.ItemMenu>
+                                    Início
+                                </C.ItemMenu>
+                                <C.ItemMenu>
+                                    Soluções
+                                </C.ItemMenu>
+                                <C.ItemMenu>
+                                    Preços
+                                </C.ItemMenu>
+                                <C.ItemMenu>
+                                    Contatos
+                                </C.ItemMenu>
+                            </C.Menu>
+                        </CgMenuRightAlt>
+                    }
+                </C.IconMenu>
+            </C.ButtonMenu>
+        </C.Nav>
     )
 }
 
---
+++ src/components/header/styles.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+    align-items: center;
+    background: rgb(24,77,30);
+    background: linear-gradient(90deg, rgba(24,77,30) 0%, rgba(134,134,134,1) 58%);
+    color: var(--cor-green-primary);
+    display: flex;
+    height: 15vh;
+    justify-content: space-between;
+    padding: 0 2rem;
+    position: fixed;
+    top:0;
+    width: 100vw;
+    z-index: 99;
+`;
+
+export const Logo = styled.img`
+    width: 150px;
+`;
+
+export const Menu = styled.ul`
+    display: flex;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    height: auto;
+
+    @media (max-width: 840px) {
+        background: rgb(24,77,30);
+        background: linear-gradient(90deg, rgba(24,77,30) 0%, rgba(134,134,134,1) 58%);
+        backdrop-filter: blur(3px);
+        display: ${({ showMenu }) => (showMenu ? 'flex' : 'none')};
+        flex-direction: column;
+        height: 100vh;
+        justify-content:start;
+        opacity: .95;
+        padding: 10px 0;
+        position: absolute;
+        right: 0;
+        top: 80px;
+        width: 100%;
+        z-index: 99;
+  }
+`;
+
+export const ItemMenu = styled.li`
+    margin: 0 1rem;
+    font-size: 1rem;
+    font-weight: bold;
+    text-transform: uppercase;
+
+    :hover{
+        color:var(--cor-light-primary);
+        cursor: pointer;
+        font-size: 1.2rem;
+    }
+
+    @media (max-width: 840px) {
+        margin:1rem auto;
+    }
+`;
+
+export const ButtonMenu = styled.div`
+  display: none;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 2rem;
+
+  @media (max-width: 840px) {
+    display:flex;
+  }
+`;
+
+export const IconMenu = styled.i`
+  svg {
+    height: 50px;
+    width: 50px;  
+  }
+  :hover{
+      color: var(--cor-primary);
+      cursor: pointer;
+    }
+`;

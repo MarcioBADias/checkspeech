ec17de35c985c2fc811d03d2adb22ae6c48e077f
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-22 11:24:39 -0300

    validando campos com yup

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -1,19 +1,35 @@
 import React from "react";
-
 import { Formik, Form, Field } from "formik";
-
+import * as Yup from 'yup';
 import * as C from './styles';
+
+const schema = Yup.object().shape({
+    fullName: Yup.string().required(),
+    email: Yup.string().required(),
+    phone: Yup.number().min(11).required()
+});
 
 const Contacts = () => {
     return(
         <C.Section id="contacts">      
-            <Formik>
-                {()=>(
+            <Formik
+                validationSchema={schema}
+                initialValues={{
+                    fullName:'',
+                    email:'',
+                    phone:'',
+                    message:''
+                }}
+            >
+                {( { errors })=>(
                     <Form>
                         <C.Container>
-                            <C.Input name='name' type='text' placeholder='Nome Completo' />
-                            <C.Input name='email' type='text' placeholder='E-Mail' />
-                            <C.Input name='phone' type='text' placeholder='Telefone' />
+                            <Field name='fullName' type='text' placeholder='Nome Completo' />
+                            {errors.fullName && (
+                                <spam style={{ color:'red' }}>Campo obrigatótio</spam>
+                            )}
+                            <Field name='email' type='text' placeholder='E-Mail' />
+                            <Field name='phone' type='text' placeholder='Telefone' />
                             <C.InputTextArea name='message' type='text' placeholder='Digite sua menssagem' />
                         </C.Container>
                     </Form>

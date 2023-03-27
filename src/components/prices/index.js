e1322c5cbd5f45c308c80857d28744914bf38e65
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-27 20:26:35 -0300

    ajuste na caixa de preços

--- src/components/prices/index.js
+++ src/components/prices/index.js
@@ -35,10 +35,30 @@ const Prices = () => {
                 <C.Button>Escoler Plano</C.Button>
             </C.PriceBox>
             <C.PriceBox>
-
+                <C.SubTitle>
+                    Beguinner
+                </C.SubTitle>
+                <C.List>
+                    <C.Item>Transcrição de áudio assíncrono <strong>120 minutos</strong> <C.Span>(R$ 0,20 / min. excedente)</C.Span></C.Item>
+                    <C.Item>Transcrição de áudio em tempo real <strong>90 minutos</strong> <C.Span>(R$ 0,40 / min. excedente)</C.Span></C.Item>
+                    <C.Item>Identificação de idiomas em áudios <strong>200 minutos</strong> <C.Span>(R$ 0,05 / min. excedente)</C.Span></C.Item>
+                    <C.Item>Análise de sentimento a partir do áudio <strong>120 minutos</strong> <C.Span>(R$ 0,40 / min. excedente)</C.Span></C.Item>
+                </C.List>
+                <C.Price>R$29,90</C.Price>
+                <C.Button>Escoler Plano</C.Button>
             </C.PriceBox>
             <C.PriceBox>
-
+                <C.SubTitle>
+                    Beguinner
+                </C.SubTitle>
+                <C.List>
+                    <C.Item>Transcrição de áudio assíncrono <strong>120 minutos</strong> <C.Span>(R$ 0,20 / min. excedente)</C.Span></C.Item>
+                    <C.Item>Transcrição de áudio em tempo real <strong>90 minutos</strong> <C.Span>(R$ 0,40 / min. excedente)</C.Span></C.Item>
+                    <C.Item>Identificação de idiomas em áudios <strong>200 minutos</strong> <C.Span>(R$ 0,05 / min. excedente)</C.Span></C.Item>
+                    <C.Item>Análise de sentimento a partir do áudio <strong>120 minutos</strong> <C.Span>(R$ 0,40 / min. excedente)</C.Span></C.Item>
+                </C.List>
+                <C.Price>R$29,90</C.Price>
+                <C.Button>Escoler Plano</C.Button>
             </C.PriceBox>
             </C.Container>
         </C.Section>
--- src/components/prices/styles.js
+++ src/components/prices/styles.js
@@ -28,39 +28,58 @@ export const Container = styled.div`
 export const PriceBox = styled.div`
     align-items: center;
     background: linear-gradient(90deg, var(--cor-light-secondary) 0%, var(--cor-light-primary) 58%);
+    border: 5px var(--cor-green-primary) solid;
     border-radius: 15px;
     color: var(--cor-dark-primary);
     display: flex;
     flex-direction: column;
     justify-content: center;
-    height: 60vh;
     padding: 2rem .5rem;
     width: 25vw;
     
     @media (max-width: 840px) {
         margin-top: 2rem;
-        width: 90vw;
+        width: 80vw;
     }
     `;
 
     export const Title = styled.h1`
         font-size: 2rem;
         padding-bottom: 1rem;
+        text-transform: uppercase;
     `;
     
-    export const SelectPlan = styled.select``;
+    export const SelectPlan = styled.select`
+        border: 5px var(--cor-green-primary) solid;
+        border-radius: 15px;
+        font-size: 1rem;
+        font-weight: bold;
+        padding: .5rem 1rem;
+    `;
 
     export const Option = styled.option``;
 
-    export const SubTitle = styled.h2``;
+    export const SubTitle = styled.h2`
+    `;
 
-    export const List = styled.ul``;
+    export const List = styled.ul`
+        list-style: none;
+        padding: 0 1rem;
+        margin-top: 1rem;
+    `;
 
-    export const Item = styled.li``;
+    export const Item = styled.li`
+        font-size: 1.2rem;
+        margin-top: .5rem;
+    `;
 
-    export const Span = styled.span``;
+    export const Span = styled.span`
+        font-size: .8rem;
+    `;
 
-    export const Price = styled.h1``;
+    export const Price = styled.h1`
+        margin-top: 1rem;
+    `;
 
     export const Button = styled.button`
         background: linear-gradient(75deg, var(--cor-light-secondary) 0%, var(--cor-green-primary) 68%);;

09d77eff2617b7691ac0b0159b393032bd974cf4
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-03-27 19:09:48 -0300

    Estruturando os componentes da section

--- src/components/prices/index.js
+++ src/components/prices/index.js
@@ -6,13 +6,30 @@ const Prices = () => {
     return(
         <C.Section id="prices">   
             <C.Container>
-
+            <C.Title>
+                Conheça nossos preços
+            </C.Title>
             </C.Container>
             <C.Container>
-
+                <C.SelectPlan>
+                    <C.Option>
+                        Mensal
+                    </C.Option>
+                    <C.Option>
+                        Anual
+                    </C.Option>
+                </C.SelectPlan>
             </C.Container>
             <C.Container>
+            <C.PriceBox>
 
+            </C.PriceBox>
+            <C.PriceBox>
+
+            </C.PriceBox>
+            <C.PriceBox>
+
+            </C.PriceBox>
             </C.Container>
         </C.Section>
     )
--- src/components/prices/styles.js
+++ src/components/prices/styles.js
@@ -1,22 +1,31 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-    min-height: 100vh;
     background: var(--cor-dark-primary);
     align-items: center;
     display: flex;
-    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
     position: relative;
+`;
+
+export const Container = styled.div`
+    background: var(--cor-dark-primary);
+    display: flex;
+    justify-content: space-around;
+    margin-top: 1rem;
+    width: 100vw;
 
     @media (max-width: 840px) {
         align-items: center;
         flex-direction: column;
         justify-content: center;
     }
-
 `;
 
-export const Container = styled.div`
+
+export const PriceBox = styled.div`
     align-items: center;
     background: linear-gradient(90deg, var(--cor-light-secondary) 0%, var(--cor-light-primary) 58%);
     border-radius: 15px;
@@ -26,15 +35,18 @@ export const Container = styled.div`
     height: 60vh;
     padding: 2rem .5rem;
     width: 30vw;
-
+    
     @media (max-width: 840px) {
         margin-top: 2rem;
         width: 90vw;
     }
-`;
+    `;
 
-export const Title = styled.h1`
-    font-size: 2rem;
-    padding-bottom: 1rem;
-`;
+    export const Title = styled.h1`
+        font-size: 2rem;
+        padding-bottom: 1rem;
+    `;
+    
+    export const SelectPlan = styled.select``;
 
+    export const Option = styled.option``;
